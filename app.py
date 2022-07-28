fa90902867c410d57bdbb36e45cb243a7ee63ec9
salumterra <guiterra@gmail.com>
2022-07-28 00:32:51 +0000

    aula4

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# lista de posts
-posts = [
-    {
-        "title": "O meu primeiro post",
-        "body": "Aqui é o texto do post",
-        "author": "Guilherme",
-        "created": datetime(2022,7,25)
-    },
-    {
-        "title": "O meu segundo post",
-        "body": "Aqui é o texto do post",
-        "author": "Feulo",
-        "created": datetime(2022,7,26)
-    }
-]
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+db.create_all()
 
 @app.route("/")
 def index():
+    posts = Post.query.all()
     return render_template("index.html", posts=posts)
+
+@app.route("/populate")
+def populate():
+    user = User(username="Terra", email="teste@teste.com", password_hash="a")
+    post1 = Post(title="Post 1", body="Texto do post 1", author=user)
+    post2 = Post(title="Post 2", body="Texto do post 2", author=user)
+    db.session.add(user)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
