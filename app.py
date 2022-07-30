0e06e5e13e8934ccfa77f66e7116fd68757c35da
salumterra <guiterra@gmail.com>
2022-07-30 00:21:50 +0000

    aula6

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
 
@@ -85,3 +85,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@app.route("/create", methods=["GET","POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        try:
+            post = Post(title = title, body=body, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for("index"))
+        except IntegrityError:
+            flsah("Error on create Post, try again later")
+    return render_template("create.html")
